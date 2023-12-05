d0b930bc6630edd61ea1b5055f840a39396945bb
Sanchez Redondo <progpabsanch@gmail.com>
2023-12-05 14:04:44 +0100

    things

--- p303/p303.py
+++ p303/p303.py
@@ -1,6 +1,24 @@
-from typing import Tuple
+from typing import Tuple, Union
 
 import numpy as np
 
 def split(t: np.ndarray)-> Tuple[np.ndarray, int, np.ndarray]:
+    ...
+
+def qsel(t: np.ndarray, k: int)-> Union[int, None]:
+    ...
+
+def qsel_nr(t: np.ndarray, k: int)-> Union[int, None]:
+    ...
+
+def split_pivot(t: np.ndarray, mid: int)-> Tuple[np.ndarray, int, np.ndarray]:
+    ...
+
+def pivot5(t: np.ndarray)-> int:
+    ...
+
+def qsel5_nr(t: np.ndarray, k: int)-> Union[int, None]:
+    ...
+
+def qsort_5(t: np.ndarray)-> np.ndarray:
     ...
